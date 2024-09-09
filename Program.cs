c2779f2ad36433d483dca8f22c42d0d3625e8e07
rsbastos <renedossantosbastos@gmail.com>
2024-09-09 11:54:47 -0300

    :feat: CRUD - Get by Id

--- Program.cs
+++ Program.cs
@@ -59,6 +59,14 @@ app.MapGet("/veiculos", ([FromQuery] int? pagina, IVeiculoServico veiculoServico
 
     return Results.Ok(veiculos);
 }).WithTags("Veiculos");
+
+app.MapGet("/veiculos/{id}", ([FromRoute] int id, IVeiculoServico veiculoServico) => {
+    var veiculo = veiculoServico.BuscaPorId(id);
+
+    if(veiculo == null) return Results.NotFound();
+
+    return Results.Ok(veiculo);
+}).WithTags("Veiculos");
 #endregion
 
 #region App
