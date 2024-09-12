c1ed2f844ef39e08cf004e9723bbec0cc85ca619
rsbastos <renedossantosbastos@gmail.com>
2024-09-12 12:34:29 -0300

    :white_check_mark: Test - request tests

--- Test/Domain/Servicos/AdministradorServico.cs
+++ Test/Domain/Servicos/AdministradorServico.cs
@@ -65,6 +65,6 @@ public class AdministradorServicoTest
         var admDoBanco = administradorServico.BuscaPorId(adm.Id);
 
         // Assert
-        Assert.AreEqual(1, admDoBanco.Id);        
+        Assert.AreEqual(1, admDoBanco?.Id);        
     }
 }
